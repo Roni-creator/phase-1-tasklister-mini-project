cdea74a4ce5c917e0388eece89dac99704c2549b
veronicah <mbuviveronicah3@gmail.com>
2023-03-24 14:08:24 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+   let form = document.querySelector('form')
+  form.addEventListener('submit',(e) =>{
+   e.preventDefault()
+     BuildToDo(e.target.new_task_description.value)
+     form.reset()
+  })
+})
+function BuildToDo(todo) {
+  let p= document.createElement('p')
+  let btn= document .querySelector('button')
+  btn.addEventListener('click',handleDelete)
+  btn.textcontent='x'
+   p.textContent=`${todo}`
+   p.appendChild(btn)
+   console.log(p)
+     document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
